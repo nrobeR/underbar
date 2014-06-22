575fb9b473ec523fd275bbef3b2520fc574fcaef
nrobeR <mr.nrober@gmail.com>
2014-06-22 16:59:18 -0400

    _.reject fixed

--- src/underbar.js
+++ src/underbar.js
@@ -94,8 +94,8 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-   return _.filter(collection, function(){
-    return !test;
+    return _.filter(collection, function(collection){
+      return !test(collection);
    });
   };
 
