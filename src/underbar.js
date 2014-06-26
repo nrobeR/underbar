3006f8054cb937ff5a55c21f99813b85b3b1f34a
nrobeR <mr.nrober@gmail.com>
2014-06-26 00:13:26 -0400

    _every stopped

--- src/underbar.js
+++ src/underbar.js
@@ -191,6 +191,10 @@ var _ = {};
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection,function(pass,item){
+      if(!pass) return false;
+      return iterator(item);
+    },true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
